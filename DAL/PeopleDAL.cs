b985097bf361552e7c7305dcff2ba77bd9833068
orel <0533orel@gmail.com>
2025-06-09 20:42:49 +0300

    add method UpdateType and GetIdByFullName in peopleDal

--- DAL/PeopleDAL.cs
+++ DAL/PeopleDAL.cs
@@ -49,7 +49,7 @@ namespace Malshinon.DAL
             try
             {
                 conn = SQLConnection.OpenConnect();
-                string Query = @"SELECT p.id FROM people p WHERE @secretCode = p.secret_code";
+                string Query = @"SELECT p.id FROM people p WHERE p.secret_code = @secretCode";
                 MySqlCommand cmd = new MySqlCommand(Query, conn);
                 cmd.Parameters.AddWithValue("@secretCode", secretCode);
                 var reader = cmd.ExecuteReader();
@@ -66,6 +66,37 @@ namespace Malshinon.DAL
                 SQLConnection.CloseConnection(conn);
             }
         }
+
+
+
+
+        public static int GetIdByFullName(string firstName, string lastMane)
+        {
+            try
+            {
+                conn = SQLConnection.OpenConnect();
+                string Query = @"SELECT p.id FROM people p WHERE p.first_name = @firstName AND p.last_name = lastMane";
+                MySqlCommand cmd = new MySqlCommand(Query, conn);
+                cmd.Parameters.AddWithValue("@firstName", firstName);
+                cmd.Parameters.AddWithValue("@lastMane", lastMane);
+                var reader = cmd.ExecuteReader();
+                reader.Read();
+                return reader.GetInt32("id");
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return -1;
+            }
+            finally
+            {
+                SQLConnection.CloseConnection(conn);
+            }
+        }
+
+
+
+
 
 
         public static void UpdateSecretCode(int id, string secretCode)
@@ -90,14 +121,17 @@ namespace Malshinon.DAL
         }
 
 
-        public static void UpdateNumReports(int secretCode)
+
+
+
+        public static void UpdateNumReports(int id)
         {
             try
             {
                 conn = SQLConnection.OpenConnect();
-                string Query = @"UPDATE people SET num_reports = num_reports + 1 WHERE @secretCode = secret_code";
+                string Query = @"UPDATE people SET num_reports = num_reports + 1 WHERE id = @id";
                 MySqlCommand cmd = new MySqlCommand(Query, conn);
-                cmd.Parameters.AddWithValue("@secretCode", secretCode);
+                cmd.Parameters.AddWithValue("@id", id);
                 cmd.ExecuteNonQuery();
             }
             catch (MySqlException ex)
@@ -111,14 +145,49 @@ namespace Malshinon.DAL
         }
 
 
+
+
+
         public static void UpdateNumMentions(int id)
         {
             try
             {
                 conn = SQLConnection.OpenConnect();
-                string Query = @"UPDATE people SET num_mentions = num_mentions + 1 WHERE @id = id";
+                string Query = @"UPDATE people SET num_mentions = num_mentions + 1 WHERE id = @id";
                 MySqlCommand cmd = new MySqlCommand(Query, conn);
                 cmd.Parameters.AddWithValue("@id", id);
+                cmd.ExecuteNonQuery();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                SQLConnection.CloseConnection(conn);
+            }
+        }
+
+
+
+
+
+        public static void UpdateType(int id, int chosenType)
+        {
+            try
+            {
+                Dictionary<int, string> choseType = new Dictionary<int, string>
+                {
+                    {1, "reporter" },
+                    {2, "target" },
+                    {3, "both" },
+                    {4, "potential agent" }
+                };
+                conn = SQLConnection.OpenConnect();
+                string Query = @"UPDATE people SET type = choseType  WHERE id = @id";
+                MySqlCommand cmd = new MySqlCommand(Query, conn);
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.Parameters.AddWithValue("@types", choseType[chosenType]);
                 cmd.ExecuteNonQuery();
             }
             catch (MySqlException ex)
