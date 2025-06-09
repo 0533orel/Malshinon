b885398dacf0941e7bc6958d7ff883672c12c271
orel <0533orel@gmail.com>
2025-06-09 16:19:38 +0300

    create people IntelReports

---
+++ Models/IntelReports.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Malshinon.Models
+{
+    public class IntelReports
+    {
+        public int ReporterId { get; set; }
+        public int TargetId { get; set; }
+        public string Text { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
